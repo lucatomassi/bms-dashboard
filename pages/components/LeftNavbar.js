e3b87f87bb8d8a633b0ec92b6073e34fa37ca24f
lucatomassi <luca@tomassi.ca>
2022-02-11 08:28:39 -0500

    mod. nav-bar

--- pages/components/LeftNavbar.js
+++ pages/components/LeftNavbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Dropdown } from './Dropdown.js'
 import {
 	faCog,
 	faSignOutAlt,
